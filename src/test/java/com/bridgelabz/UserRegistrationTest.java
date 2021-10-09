8c5079b4ba2741566b0fd1dedc00da9d9e98a6c6
atif00 <mohammedatif90@gmail.com>
2021-10-09 19:34:10 +0530

    Refactor

--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -1,15 +1,17 @@
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserRegistrationTest {
+    Scanner scanner = new Scanner(System.in);
     @Test
     public void validateUserFirstName() {
-        assertEquals(true, UserRegistration.isValidFirstName());
+        UserRegistration userRegistration = new UserRegistration();
+
+        boolean result = userRegistration.isValidFirstName("Mohammed");
+        assertEquals(true, result);
     }
 
 }

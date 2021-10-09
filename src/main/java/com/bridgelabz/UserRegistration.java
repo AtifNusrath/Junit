1ce3d411f5ce2fc4c870b3ac085dcde16306397c
atif00 <mohammedatif90@gmail.com>
2021-10-09 20:18:36 +0530

    Password validation

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,10 +5,9 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static boolean isValidFirstName() {
+    public static boolean isValidFirstName(String firstName) {
+        String pattern = "^[A-Z][a-z]{3,}";
 
-        String firstName = "Mohammed";
-        String pattern = "^[A-Z][A-Za-z]{2,}";
         Pattern fnme = Pattern.compile(pattern);
         if (firstName == null) {
             return false;
@@ -17,9 +16,9 @@ public class UserRegistration {
         return match.matches();
     }
 
-    public static boolean isValidLastName() {
-        String lastName = "Atif";
-        String lname = "^[A-Z][A-Za-z]{3,}";
+    public static boolean isValidLastName(String lastName) {
+        String lname = "^[A-Z][a-z]{3,}";
+
         Pattern pattern = Pattern.compile(lname);
         if (lastName == null) {
             return false;
@@ -28,20 +27,17 @@ public class UserRegistration {
         return m2.matches();
     }
 
-    public static boolean isValidEmail() {
+    public static boolean isValidEmail(String emails) {
 
-        String emails = "user@domain.com";
         String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
         Pattern pattern = Pattern.compile(regex);
         if (emails == null) {
             return false;
         }
-
         Matcher matcher = pattern.matcher(emails);
         return matcher.matches();
     }
-    public static boolean isValidNo() {
-        String mobileNo = "91 9874563210";
+    public static boolean isValidMobileNo(String mobileNo) {
         String mobile = "^(\\d{0,9}[\\s]?)?\\d{10}$";
         Pattern mob = Pattern.compile(mobile);
         if (mobileNo == null) {
@@ -52,9 +48,8 @@ public class UserRegistration {
         return matcher.matches();
     }
 
-    public static boolean isValidPwd()
+    public static boolean isValidPwd(String pwd)
     {
-        String pwd = "Adminpassword";
         String password = "^[A-Za-z]{8,}$";
         Pattern pattern = Pattern.compile(password);
         if (pwd ==null) {
